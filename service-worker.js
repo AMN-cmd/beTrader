34555fb861399b58eb70e9a650f4bb8a3f673efa
AMN-cmd <unitedsolar88@gmail.com>
2025-12-30 23:27:28 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'betrader-v2';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './pdf-generator.js',
+    './favicon.ico',
+    './icons/betrader-192x192.png',
+    './icons/betrader-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+            .catch(err => {
+                console.log('Cache install error:', err);
+            })
+    );
+    self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch event - Network first, fallback to cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
